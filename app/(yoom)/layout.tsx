827a81ef43897917617ff7d485ce0724892d45b8
Daler-web-dev <dsarifkulo021@gmail.com>
2024-05-22 15:22:06 +0500

    first commit

---
+++ app/(yoom)/layout.tsx
@@ -0,0 +1,13 @@
+
+
+
+
+export default function DashboardLayout({children}: {children: React.ReactNode}) {
+    return (
+        <section>
+            <header></header>
+            <aside></aside>
+            {children}
+        </section>
+    )
+}
